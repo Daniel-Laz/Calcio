c3c892910f60ab3480d0828ad87bc0ec0941c68f
Lazzaroni.21130 <lazzaroni.daniel.studente@itispaleocapa.it>
2024-10-07 13:00:58 +0200

    metodo addCalciatore

--- src/calcio/Main.java
+++ src/calcio/Main.java
@@ -1,7 +1,24 @@
 package calcio;
 
 public class Main {
+    public static Calciatore[] calciatori= new Calciatore[100];
+    public static int contatore;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
+
+    public static boolean addCalciatore(String name, int goal, boolean capitano){
+        if (capitano){
+            for (int i = 0; i< calciatori.length; i++){
+                if (calciatori[i].isCapitano()){
+                    return false;
+                }
+            }
+        }
+        calciatori[contatore]=new Calciatore(name, goal, capitano);
+        contatore++;
+        return true;
+    }
+
 }
