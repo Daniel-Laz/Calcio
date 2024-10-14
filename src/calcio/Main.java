a7416f7d3f039c4ac2b235bedff9131d2e28d9e4
Lazzaroni.21130 <lazzaroni.daniel.studente@itispaleocapa.it>
2024-10-14 12:28:24 +0200

    creazione oggetto squadra (separazione metodi della squadra e del main)

--- src/calcio/Main.java
+++ src/calcio/Main.java
@@ -3,13 +3,12 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static Calciatore[] calciatori= new Calciatore[100];
-    public static int contatore;
 
     public static void main(String[] args) {
+        Squadra squadra = new Squadra();
         Scanner in = new Scanner(System.in);
         int selezione;
-        do{
+        do {
             System.out.println("menu:\n" +
                     "1. aggiunta giocatore\n" +
                     "2. visualizza squadra\n" +
@@ -17,114 +16,44 @@ public class Main {
                     "4. cancella giocatore\n" +
                     "5. visualizza giocatori con > 5 goal\n" +
                     "6. visualizza capitano - se non ci sono capitani ne assegna uno casualmente");
-                    selezione = in.nextInt();
+            selezione = in.nextInt();
 
-                    switch(selezione){
-                        case 1:
-                            System.out.println("inserisci in ordine: nome INVIO goal INVIO capitano? (true o false)");
-                            if (addCalciatore(in.next(), in.nextInt(),in.nextBoolean())){
-                                System.out.println("giocatore aggiunto");
-                            }else{
-                                System.out.println("giocatore non aggiunto - è già presente un capitano");
-                            }
-                            break;
-                        case 2:
-                            System.out.println(printSquadra());
-                            break;
-                        case 3:
-                            System.out.println("inserisci in ordine: nome_precedente INVIO\nnome_nuvo INVIO goal_nuovi INVIO capitano? (true o false)");
-                            if(editGiocatore(in.next(),in.next(),in.nextInt(),in.nextBoolean())){
-                                System.out.println("giocatore modificato");
-                            }else{
-                                System.out.println("giocatore non trovato");
-                            }
-                            break;
-                        case 4:
-                            System.out.println("inserisci il nome del giocatore da eliminare");
-                            if (delGiocatore(in.next())){
-                                System.out.println("giocatore eliminato");
-                            }else{
-                                System.out.println("giocatore non trovato");
-                            }
-                            break;
-                        case 5:
-                            System.out.println(goal5());
-                            break;
-                        case 6:
-                            System.out.println(getCapitano());
-                            break;
-                        default:
+            switch (selezione) {
+                case 1:
+                    System.out.println("inserisci in ordine: nome INVIO goal INVIO capitano? (true o false)");
+                    if (squadra.addCalciatore(in.next(), in.nextInt(), in.nextBoolean())) {
+                        System.out.println("giocatore aggiunto");
+                    } else {
+                        System.out.println("giocatore non aggiunto - è già presente un capitano");
                     }
-
-
-
-        }while(selezione!=0);
-
-    }
-
-    public static boolean addCalciatore(String name, int goal, boolean capitano){
-        if (capitano){
-            for (int i = 0; i< contatore; i++){
-                if (calciatori[i].isCapitano()){
-                    return false;
-                }
+                    break;
+                case 2:
+                    System.out.println(squadra.printSquadra());
+                    break;
+                case 3:
+                    System.out.println("inserisci in ordine: nome_precedente INVIO\nnome_nuvo INVIO goal_nuovi INVIO capitano? (true o false)");
+                    if (squadra.editGiocatore(in.next(), in.next(), in.nextInt(), in.nextBoolean())) {
+                        System.out.println("giocatore modificato");
+                    } else {
+                        System.out.println("giocatore non trovato");
+                    }
+                    break;
+                case 4:
+                    System.out.println("inserisci il nome del giocatore da eliminare");
+                    if (squadra.delGiocatore(in.next())) {
+                        System.out.println("giocatore eliminato");
+                    } else {
+                        System.out.println("giocatore non trovato");
+                    }
+                    break;
+                case 5:
+                    System.out.println(squadra.goal5());
+                    break;
+                case 6:
+                    System.out.println(squadra.getCapitano());
+                    break;
+                default:
             }
-        }
-        calciatori[contatore]=new Calciatore(name, goal, capitano);
-        contatore++;
-        return true;
-    }
-    public static String printSquadra(){
-        String out="";
-        for (int i = 0; i < contatore ; i++){
-            out+= calciatori[i].getName()+" goal:"+calciatori[i].getGoal()+" capitano:"+calciatori[i].isCapitano()+"\n";
-        }
-        return out;
-    }
-    public static boolean editGiocatore(String name, String newName, int newGoal, boolean newCapitano){
-        for (int i= 0; i< contatore; i++){
-            if (name.equalsIgnoreCase(calciatori[i].getName())){
-                calciatori[i] = new Calciatore(newName,newGoal,newCapitano);
-                return true;
-            }
-        }
-        return false;
-    }
-    public static boolean delGiocatore(String name){
-        for (int i= 0; i< contatore; i++){
-            if (name.equalsIgnoreCase(calciatori[i].getName())){
-                for (int j=i; j<contatore-1; j++) {
-                    calciatori[j] = calciatori[j + 1];
-                }
-                calciatori[contatore]=null;
-                contatore--;
-                return true;
-            }
-        }
-        return false;
-    }
-    public static String goal5(){
-        String out="";
-        for (int i = 0; i < contatore ; i++){
-            if(calciatori[i].getGoal()>5){
-                out+= calciatori[i].getName()+" goal:"+calciatori[i].getGoal()+" capitano:"+calciatori[i].isCapitano()+"\n";
-            }
-        }
-        return out;
-    }
-    public static String getCapitano(){
-        for (int i = 0; i < contatore; i++){
-            if (calciatori[i].isCapitano()){
-                return calciatori[i].getName();
-            }
-        }
-        //se non ci sono capitani chiama il metodo che assegna il ruolo capitano casualmente e ritorna il suo nome
-        return calciatori[setCapitanoRandom()].getName();
-    }
-    public static int setCapitanoRandom(){
-        Random random = new Random();
-        int casuale = random.nextInt(contatore);
-        calciatori[casuale].setCapitano(true);
-        return casuale;
+        } while (selezione != 0);
     }
 }
